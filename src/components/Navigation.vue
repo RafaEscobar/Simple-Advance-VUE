0fed8abebbd58d2326ef72399b539dbeac7bfe11
rafa <rescobar@bit.lat>
2024-12-09 11:34:17 -0600

    Agrega vista home

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <div class="px-6 py-4 border-b-2 border-black">
+    <div class="px-6 py-4 border-b border-black">
         <div class="w-1/4 flex justify-around">
             <RouterLink to="form-view">
                 <span class="font-semibold text-xl hover:text-blue-600 cursor-pointer active:text-blue-400">Avance</span>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+    <h1>Vista principal</h1>
+</template>
+
+<style scoped>
+</style>
